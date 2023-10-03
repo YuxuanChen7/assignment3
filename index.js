b527868e1b65468243075b72a5fed098ff681218
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-10-03 02:56:22 -0400

    adding comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const grid = document.getElementById("grid");
 
-//Question 1
+/*
+Question 1
+Expected: add a new row to the grid
+*/
 document.getElementById("addRow").addEventListener("click", () => {
     const newRow = grid.insertRow();
     const currentColumns = grid.rows[0]?.cells.length || 0;
@@ -10,7 +13,10 @@ document.getElementById("addRow").addEventListener("click", () => {
     }
 });
 
-//Question 2
+/*
+Question 2
+Expected: add a new col to the grid
+*/
 document.getElementById("addColumn").addEventListener("click", () => {
     for (let row of grid.rows) {
         row.insertCell();
